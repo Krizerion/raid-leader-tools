5f17fcace04d59ca296745b6f7fd914694a761b8
Kamen Yanakiev <kamen_yanakiev@epam.com>
2020-09-01 10:23:32 +0300

    Clean code configs

--- src/app/raid-planner/components/planner/planner.component.ts
+++ src/app/raid-planner/components/planner/planner.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { SPECS_IMG, CLASSES_IMG, ROLES_IMG } from '@app/shared/constants/classes-img-paths.constants';
-import { CLASSES_NAMES } from '@app/shared/constants/classes-names.constsnts';
-import { NzModalService } from 'ng-zorro-antd/modal';
 import { AddPlayerComponent } from '@app/raid-planner/components/add-player/add-player.component';
+import { CLASSES_IMG, ROLES_IMG, SPECS_IMG } from '@app/shared/constants/classes-img-paths.constants';
+import { CLASSES_NAMES } from '@app/shared/constants/classes-names.constsnts';
 import { PlannerApiService } from '@app/shared/services/planner-api.service';
+import { NzModalService } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'rv-planner',
@@ -22,7 +22,7 @@ export class PlannerComponent {
     this.players = this.plannerApiService.getPlayers();
   }
 
-  openAddNewPlayerModal() {
+  openAddNewPlayerModal(): void {
     this.modal.create({
       nzTitle: 'This is a notification message',
       nzContent: AddPlayerComponent,
